edc7f9d38c17dc3aa7953b9c732d07d32f203f09
Raphael Matori <raphael.rocha@urbam.com.br>
2020-08-19 12:16:34 -0300

    Initial commit

---
+++ src/test/java/br/com/raphaelmatori/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raphaelmatori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
